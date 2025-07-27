471c5e821b58b15c23a1792a10216cb6d08b44ac
sidendev <96108993+sidendev@users.noreply.github.com>
2025-07-27 18:00:37 +0100

    Set up 1.sql for tables to be created, update HomeController and update routes from initial template

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,9 +1,27 @@
 # --- !Ups
-CREATE TABLE users (
+
+CREATE TABLE employees (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255),
-  email VARCHAR(255) UNIQUE
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  mobile_number VARCHAR(20) NOT NULL,
+  address VARCHAR(255)
+);
+
+CREATE TABLE contracts (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  employee_id INT NOT NULL,
+  contract_type ENUM('Permanent', 'Contract') NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE,
+  full_time BOOLEAN NOT NULL DEFAULT true,
+  hours_per_week INT,
+  FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE
 );
 
 # --- !Downs
-DROP TABLE users;
+
+DROP TABLE contracts;
+DROP TABLE employees;
+
